5a69dec483d66c5c2d68941701d2d2e2bae52494
Sreehari S <sreeharisanthosh841@gmail.com>
2022-11-30 18:19:40 +0530

    Updates

---
+++ precache-manifest.c7b06793ff0f94131c1db137cb34de31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2445f4a6b0e7b07340dc",
+    "url": "/v-chat/static/css/main.0dd02d26.chunk.css"
+  },
+  {
+    "revision": "2445f4a6b0e7b07340dc",
+    "url": "/v-chat/static/js/main.2445f4a6.chunk.js"
+  },
+  {
+    "revision": "bc20d6d6d939309784cb",
+    "url": "/v-chat/static/js/1.bc20d6d6.chunk.js"
+  },
+  {
+    "revision": "7f13834102207b689c10",
+    "url": "/v-chat/static/js/2.7f138341.chunk.js"
+  },
+  {
+    "revision": "d448d5e997c5f83cee72",
+    "url": "/v-chat/static/js/runtime~main.d448d5e9.js"
+  },
+  {
+    "revision": "cf837af4687296842e9618fdfa2a4492",
+    "url": "/v-chat/index.html"
+  }
+];
